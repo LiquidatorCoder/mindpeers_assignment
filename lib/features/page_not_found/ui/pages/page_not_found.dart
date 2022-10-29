84ef7940eb90ab50bd1d284ca9868f6bc680810b
LiquidatorCoder <maurya.abhay30@gmail.com>
2022-10-30 00:12:56 +0530

    update error page

--- lib/features/page_not_found/ui/pages/page_not_found.dart
+++ lib/features/page_not_found/ui/pages/page_not_found.dart
@@ -1,27 +1,61 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class PageNotFoundScreen extends StatelessWidget {
   final String routeName;
 
   const PageNotFoundScreen(this.routeName, {super.key});
 
+  Future<String> getRandomImage() async {
+    // Fetch dog image from 'https://dog.ceo/api/breeds/image/random'
+    // and return the image url
+    final response =
+        await http.get(Uri.parse('https://dog.ceo/api/breeds/image/random'));
+    return jsonDecode(response.body)['message'];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Page not found!'),
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            FutureBuilder<String>(
+              future: getRandomImage(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(
+                      snapshot.data!,
+                    ),
+                    radius: 100,
+                    backgroundColor: Colors.transparent,
+                  );
+                }
+                return const CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.transparent,
+                );
+              },
+            ),
+            const SizedBox(height: 24),
             Container(
               padding: const EdgeInsets.all(8),
               child: const Text(
-                'Oops! Page not found!',
-                style: TextStyle(fontSize: 18),
+                'Oops!\nPage not found!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
+            const SizedBox(height: 24),
           ],
         ),
       ),
